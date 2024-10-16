dd4fb83695b2889c0e09a640ddc4ee3fff4d76bc
Duy Hai <145411748+haitd2810@users.noreply.github.com>
2024-10-16 15:45:01 +0700

    Update DAO

---
+++ Coffee_Shop/DAO/TableDAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffee_Shop.DAO
+{
+    internal class TableDAO
+    {
+    }
+}
